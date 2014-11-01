bcc8f5d226fb6ed8ba7eccd85ab7ecda1eec6c09
Cristian Gomez <contact@cristiangomez.me>
2014-11-01 13:44:04 -0400

    instantiate volley singleton at splash

--- app/src/main/java/us/axelia/axelia/SplashScreenActivity.java
+++ app/src/main/java/us/axelia/axelia/SplashScreenActivity.java
@@ -1,18 +1,32 @@
 package us.axelia.axelia;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
-public class SplashScreenActivity extends Activity  {
+public class SplashScreenActivity extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        VolleyQueue.getInstance(getApplicationContext());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 
 
@@ -29,9 +43,6 @@ public class SplashScreenActivity extends Activity  {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 }
